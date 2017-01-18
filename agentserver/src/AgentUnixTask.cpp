eacba7beec3b2c59b7e8ee321d41ee90b4a2bdaa
wangliming <245432668@qq.com>
2017-01-18 14:56:31 +0800

    Unixtask

---
+++ agentserver/src/AgentUnixTask.cpp
@@ -0,0 +1,58 @@
+
+/**
+ * Copyright (C) Anny Wang.
+ * Copyright (C) Hupu, Inc.
+ */
+
+#include "AgentUnixTask.h"
+#include "AgentConfig.h"
+#include "SvrCmd.h"
+
+AgentUnixTask::AgentUnixTask(wSocket *socket, int32_t type) : wUnixTask(socket, type) {
+	On(CMD_SVR_REQ, SVR_REQ_GXID, &AgentUnixTask::GetSvrByGXid, this);
+	On(CMD_SVR_REQ, SVR_REQ_REPORT, &AgentUnixTask::ReportSvr, this);
+}
+
+// 客户端查询请求
+int AgentUnixTask::GetSvrByGXid(struct Request_t *request) {
+	struct SvrReqGXid_t* cmd = reinterpret_cast<struct SvrReqGXid_t*>(request->mBuf);
+	AgentConfig* config = Config<AgentConfig*>();
+
+	struct SvrOneRes_t vRRt;
+	vRRt.mSvr.mGid = cmd->mGid;
+	vRRt.mSvr.mXid = cmd->mXid;
+	if (config->Qos()->QueryNode(vRRt.mSvr).Ok()) {
+		vRRt.mCode = 0;
+		vRRt.mNum = 1;
+		AsyncSend(reinterpret_cast<char *>(&vRRt), sizeof(vRRt));
+
+		// 同步其他worker进程
+		memcpy(cmd->mHost, vRRt.mSvr.mHost, kMaxHost);
+		cmd->mPort = vRRt.mSvr.mPort;
+		SyncWorker(reinterpret_cast<char*>(cmd), sizeof(struct SvrReqGXid_t));
+	} else {
+		vRRt.mCode = -1;
+		vRRt.mNum = 0;
+		AsyncSend(reinterpret_cast<char *>(&vRRt), sizeof(vRRt));
+	}
+	return 0;
+}
+
+// 客户端发来上报请求
+int AgentUnixTask::ReportSvr(struct Request_t *request) {
+	struct SvrReqReport_t* cmd = reinterpret_cast<struct SvrReqReport_t*>(request->mBuf);
+	AgentConfig* config = Config<AgentConfig*>();
+
+	struct SvrResReport_t vRRt;
+	if (config->Qos()->CallerNode(cmd->mCaller).Ok()) {
+		vRRt.mCode = 0;
+		AsyncSend(reinterpret_cast<char *>(&vRRt), sizeof(vRRt));
+
+		// 同步其他worker进程
+		SyncWorker(reinterpret_cast<char*>(cmd), sizeof(struct SvrReqReport_t));
+	} else {
+		vRRt.mCode = -1;
+		AsyncSend(reinterpret_cast<char *>(&vRRt), sizeof(vRRt));
+	}
+	return 0;
+}
---
+++ agentserver/src/AgentUnixTask.h
@@ -0,0 +1,25 @@
+
+/**
+ * Copyright (C) Anny Wang.
+ * Copyright (C) Hupu, Inc.
+ */
+
+#ifndef _AGENT_UNIX_TASK_H_
+#define _AGENT_UNIX_TASK_H_
+
+#include "wCore.h"
+#include "wStatus.h"
+#include "wMisc.h"
+#include "wUnixTask.h"
+
+using namespace hnet;
+
+class AgentUnixTask : public wUnixTask {
+public:
+	AgentUnixTask(wSocket *socket, int32_t type);
+
+	int GetSvrByGXid(struct Request_t *request);
+	int ReportSvr(struct Request_t *request);
+};
+
+#endif
