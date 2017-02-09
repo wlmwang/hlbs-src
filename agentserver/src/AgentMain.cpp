4c7ad5a9ae49492bffb3c23a5bf8f0d324211587
wangliming <245432668@qq.com>
2017-02-09 13:26:45 +0800

    运行宿主目录

--- agentserver/src/AgentMain.cpp
+++ agentserver/src/AgentMain.cpp
@@ -29,10 +29,12 @@ int main(int argc, const char *argv[]) {
 		std::cout << s.ToString() << std::endl;
 		return -1;
 	}
-
+	if (misc::SetBinPath() == -1) {
+		std::cout << "set bin path failed" << std::endl;
+	}
 	// 设置相关相关配置
 	soft::SetSoftName("HLBS(*agent*) -");
-	soft::SetSoftVer("2.3.0");
+	soft::SetSoftVer("2.3.2");
 	soft::SetLockPath("../log/hlbs.lock");
 	soft::SetPidPath("../log/hlbs.pid");
 	soft::SetLogPath("../log/hlbs.log");
