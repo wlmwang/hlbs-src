55f50a50888e07deac1c2c12b4e9ec3c103e26aa
wangliming <245432668@qq.com>
2017-05-23 11:59:34 +0800

    提取公共版本信息

---
+++ common/Define.h
@@ -0,0 +1,17 @@
+
+/**
+ * Copyright (C) Anny Wang.
+ * Copyright (C) Hupu, Inc.
+ */
+
+#ifndef _COMMON_DEFINE_H_
+#define _COMMON_DEFINE_H_
+
+const char 	kHlbsSoftwareName[]   = "HLBS";
+const char 	kHlbsSoftwareVer[]    = "3.0.3";
+
+const char 	kHlbsLockPath[] = "../log/hlbs.lock";
+const char 	kHlbsPidPath[] = "../log/hlbs.pid";
+const char 	kHlbsLogPath[] = "../log/hlbs.log";
+
+#endif
