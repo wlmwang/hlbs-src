d1ab0ac2577eda57da9bd5970bbd110df2e8015f
wangliming <245432668@qq.com>
2017-06-01 16:30:39 +0800

    router channel task

---
+++ routerserver/src/RouterChannelTask.cpp
@@ -0,0 +1,42 @@
+
+/**
+ * Copyright (C) Anny Wang.
+ * Copyright (C) Hupu, Inc.
+ */
+
+#include "RouterChannelTask.h"
+#include "RouterConfig.h"
+#include "RouterServer.h"
+#include "SvrCmd.h"
+
+RouterChannelTask::RouterChannelTask(wSocket *socket, wMaster *master, int32_t type) : wChannelTask(socket, master, type) {
+	On(CMD_SVR_RES, SVR_RES_RELOAD, &RouterChannelTask::ReloadSvrRes, this);
+	On(CMD_SVR_RES, SVR_RES_SYNC, &RouterChannelTask::SyncSvrRes, this);
+}
+
+int RouterChannelTask::ReloadSvrRes(struct Request_t *request) {
+	struct SvrResReload_t* cmd = reinterpret_cast<struct SvrResReload_t*>(request->mBuf);
+	RouterConfig* config = Config<RouterConfig*>();
+	if (cmd->mCode >= 0 && cmd->mNum > 0) {
+		if (cmd->mCode == 0) {	// 清除原始svr
+			config->Qos()->CleanNode();
+		}
+		for (int32_t i = 0; i < cmd->mNum; i++) {
+			config->Qos()->SaveNode(cmd->mSvr[i]);
+		}
+		Server<RouterServer*>()->Broadcast(reinterpret_cast<char*>(cmd), sizeof(struct SvrResReload_t));
+	}
+	return 0;
+}
+
+int RouterChannelTask::SyncSvrRes(struct Request_t *request) {
+	struct SvrResSync_t* cmd = reinterpret_cast<struct SvrResSync_t*>(request->mBuf);
+	RouterConfig* config = Config<RouterConfig*>();
+	if (cmd->mCode >= 0 && cmd->mNum > 0) {
+		for (int32_t i = 0; i < cmd->mNum; i++) {
+			config->Qos()->SaveNode(cmd->mSvr[i]);
+		}
+		Server<RouterServer*>()->Broadcast(reinterpret_cast<char*>(cmd), sizeof(struct SvrResSync_t));
+	}
+	return 0;
+}
---
+++ routerserver/src/RouterChannelTask.h
@@ -0,0 +1,26 @@
+
+/**
+ * Copyright (C) Anny Wang.
+ * Copyright (C) Hupu, Inc.
+ */
+
+#ifndef _ROUTER_CHANNEL_TASK_H_
+#define _ROUTER_CHANNEL_TASK_H_
+
+#include "wCore.h"
+#include "wStatus.h"
+#include "wMisc.h"
+#include "wMaster.h"
+#include "wChannelTask.h"
+
+using namespace hnet;
+
+class RouterChannelTask : public wChannelTask {
+public:
+	RouterChannelTask(wSocket *socket, wMaster *master, int32_t type = 0);
+
+	int ReloadSvrRes(struct Request_t *request);
+	int SyncSvrRes(struct Request_t *request);
+};
+
+#endif
